8d3ad9ab9b8df045a3daa1c152a762671ba89221
Patricio A. Gallardo <patricioandradeh@gmail.com>
2023-12-07 15:47:19 -0300

    Cambios en kelvin_convertidor

---
+++ basico_0/kelvin_convertidor.py
@@ -0,0 +1,12 @@
+# convertir grados celcius a grados kelvin usando funciones y variables
+# grados kelvin = grados celcius +  273.15
+
+# obtener grados celcius
+
+grados_celsius = input("ingresa los grados celcius: ")
+suma = float(grados_celsius) + 273.15
+print(suma)
+
+# hacer la suma
+# guardar el resultado en una variable
+# mostrar en la terminal el resultado
