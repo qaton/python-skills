274c90bcdd68ef3d4f539cb535257aced984ab74
Patricio A. Gallardo <patricioandradeh@gmail.com>
2023-12-12 16:28:31 -0300

    Agrega nuevos archivos

---
+++ basico_0/bucle_for.py
@@ -0,0 +1,5 @@
+"""Function python version."""
+
+nums = [2, 78, 9, 84]
+for n in nums:
+    print(n)
---
+++ basico_0/valores_items.py
@@ -0,0 +1,5 @@
+"""Function python version."""
+
+valores = {'A': 4, 'E': 3, 'I': 1, 'O': 0}
+for k, v in valores.items():
+    print('k=', k, ', v=', v)
