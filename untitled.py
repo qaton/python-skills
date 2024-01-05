96a9b410303726a3d28d270bdc40042dd378703e
GitTestingPat <patricioandradeh@gmail.com>
2024-01-05 15:16:51 -0300

    Eliminar untitled.py

--- untitled.py
+++
@@ -1,7 +0,0 @@
-def format_data(account):
-    """ Toma los datos de la cuenta y devuelve el formato imprimible. """
-    account_name = account["name"]
-    account_descr = account["description"]
-    account_country = account["country"]
-    return f"{account_name}, a {account_descr}, from {account_country}"
-    
